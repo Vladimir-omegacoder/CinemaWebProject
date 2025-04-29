69a27331ac9cbea7ac308e51d78ed1ec75bb67a0
Vladimir <sushinskyvladimir@gmail.com>
2025-04-29 13:58:53 +0300

    Account view for customer

--- Server/Controllers/CustomerController.cs
+++ Server/Controllers/CustomerController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Server.Models.DataModel;
 using Server.Models.DbEntity;
 using Server.Repositories;
 using Server.Services;
@@ -15,23 +17,57 @@ namespace Server.Controllers
     [Authorize]
     public class CustomerController : Controller
     {
+        private readonly IUserRepository _userRepository;
         private readonly ICustomerRepository _customerRepository;
+        private readonly IMovieInfoRepository _movieInfoRepository;
 
-        public CustomerController(ICustomerRepository customerRepository)
+        public CustomerController(IUserRepository userRepository, ICustomerRepository customerRepository, IMovieInfoRepository movieInfoRepository)
         {
+            _userRepository = userRepository;
             _customerRepository = customerRepository;
+            _movieInfoRepository = movieInfoRepository;
         }
 
         [HttpGet]
-        public IActionResult Home()
+        public async Task<IActionResult> Home()
+        {
+            return View(await _movieInfoRepository.GetAllAsync());
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Account()
+        {
+            string? value = User.Claims?.FirstOrDefault(c => c.Type == "UserId")?.Value;
+            if (string.IsNullOrEmpty(value))
+            {
+                return NotFound();
+            }
+
+            int userId = int.Parse(value);
+
+            var user = await _userRepository.GetAsync(userId);
+            var customer = await _customerRepository.GetAsync(userId);
+
+            var username = user?.Username ?? "No username";
+            var email = user?.Email ?? "No email";
+            var phone = customer?.Phone ?? "No phone";
+
+            var model = new CustomerInfo
+            {
+                Username = username,
+                Email = email,
+                Password = "********",
+                Role = "Customer",
+                Phone = phone,
+            };
+
+            return View("Account", model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Tickets()
         {
             return View();
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> Bookings()
-        {
-            return View("Home");
         }
     }
 }
